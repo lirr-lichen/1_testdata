18648e3dc5377331f7032e45dc37dada9e09bf65
lirr-lichen <lirr-lichen@github.com>
2020-07-27 09:09:04 +0800

    addauto

--- autotransfer_bj.sh
+++ autotransfer_bj.sh
@@ -6,7 +6,7 @@ do
 	donedir="/nfs/NGS_project/batchdone"
 	logsdir="/nfs/NGS_project/logs"
 	bcl2fastqpath="/nfs/software/bin/bcl2fastq"
-	workflowpath="/nfs/database/ngs_pipeline/snakefile-ngs-V1/"
+	workflowpath="/nfs/database/ngs_pipeline/snakefile-ngs_v1/"
 
 	for i in `ls -d ${bcldir}/*`;do
 		runid=`basename ${i}`
@@ -20,28 +20,28 @@ do
 #PBS -e ${logsdir}/${runid}.err
 #PBS -l walltime=120:00:00
 
-echo $(data +%F%n%T)
-#${bcl2fastqpath} -i ${bcldir}/${runid}/Data/Intensities/BaseCalls/ --interop-dir ${bcldir}/${runid}/InterOp/ -R ${bcldir}/${runid} --stat-dir ${bcldir}/${runid}/Stats/ --sample-sheet  ${bcldir}/${runid}/SampleSheet.csv --barcode-mismatches 0 --no-lane-splitting -o ${outdir}/${runid}
+echo \$(data +%F%n%T)
+#${bcl2fastqpath} -i ${bcldir}/${runid}/Data/Intensities/BaseCalls/ --interop-dir ${bcldir}/${runid}/InterOp/ -R ${bcldir}/${runid} --stats-dir ${bcldir}/${runid}/Stats/ --sample-sheet  ${bcldir}/${runid}/SampleSheet.csv --barcode-mismatches 0 --no-lane-splitting -o ${outdir}/${runid}
 ${bcl2fastqpath} -R ${bcldir}/${runid} -o ${outdir}/${runid} --sample-sheet ${bcldir}/${runid}/SampleSheet.csv --barcode-mismatches 0 --no-lane-splitting
-echo $(data +%F%n%T)
+echo \$(data +%F%n%T)
 python3 ${workflowpath}/run_workflow.py -W ${outdir}/${runid}_workdir -I ${outdir}/${runid}/CNS/ -O ${outdir}/${runid}_result -R ${outdir}/${runid}_report --workflow ${workflowpath} --samplesheet ${bcldir}/${runid}/SampleSheet.csv
-echo $(data +%F%n%T)
+echo \$(data +%F%n%T)
 snakemake --cores 72 --snakefile ${workflowpath}/Snakefile --restart-times 1 --rerun-incomplete --configfile ${outdir}/${runid}_workdir/config.yaml
-echo $(data +%F%n%T)  >${logsdir}/${runid}.pbs
+echo \$(data +%F%n%T)  >${logsdir}/${runid}.pbs
 
 				sleep 10
 				cd ${logsdir} && chmod 755 ${logsdir}/${runid}.pbs && qsub ${logsdir}/${runid}.pbs;
 				sleep 60 
 				while [ ! -z "`qstat`" ]
 				do
-					sleep 20 !
+					sleep 20s
 				done &&
 				touch ${donedir}/${runid}.done
 
-				rsync -acqzP ${outdir}/${runid}_report ngsdata@172.16.157.66:/gpfsdata/home/ngsdata/SY/ &&
+				rsync -acqzP ${outdir}/${runid}_report ngsdata@172.16.157.66:/gpfsdata/home/ngsdata/SY/ 
 			fi
-			else:
-				sleep 10m
+			else
+				sleep 10s
 		fi
 	done
 done
